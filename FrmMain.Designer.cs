127b037586d35800d20f9d3e3eaf75a12c0a02ad
duyluc <duongduyluc3@gmail.com>
2022-05-09 22:21:21 +0700

    Fisrt Comment

--- FrmMain.Designer.cs
+++ FrmMain.Designer.cs
@@ -29,10 +29,17 @@ namespace DataMan.Ver1
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "FrmMain";
+            this.Text = "TADA AUTOMATION";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
